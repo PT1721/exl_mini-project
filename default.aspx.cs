56c8e9da9de57ea458f937be86f33ee718d1c770
Praneet Thakur <56348797+PT1721@users.noreply.github.com>
2023-08-09 16:12:45 +0530

    Update default.aspx.cs

--- default.aspx.cs
+++ default.aspx.cs
@@ -13,5 +13,24 @@ namespace WebApp2
         {
 
         }
+         protected void Button2_Click(object sender, EventArgs e)
+        {
+            int userid = Convert.ToInt32(TextBox1.Text);
+            String password = TextBox2.Text;
+
+
+
+            MySqlConnection conn = new MySqlConnection("host=localhost;username=root;password=Praneet@1721;database=BloodBank");
+            conn.Open();
+
+            string query = $"select user_id, password from Donors where (user_id = {userid} AND  password ='{password}');";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            cmd.ExecuteNonQuery();
+
+
+
+            Response.Write("<script> alert('Login  succesfully');</script>");
+
+        }
     }
 }
