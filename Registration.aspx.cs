932176a61a60b80a7e93cb7b8330f43ff13a5b37
Praneet Thakur <56348797+PT1721@users.noreply.github.com>
2023-08-09 15:41:35 +0530

    Add files via upload

---
+++ Registration.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebApp2
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string name = TextBox1.Text;
+            string BloodG = DropDownList1.Text;
+            string Mobile = TextBox2.Text;
+            string country = DropDownList2.Text;
+            string state = DropDownList3.Text;
+            string district = DropDownList4.Text;
+            string city = DropDownList5.Text;
+
+            string Email = TextBox3.Text;
+            int ID = Convert.ToInt32(TextBox4.Text);
+            string PassW = TextBox5.Text;
+            string Re_PassW = TextBox6.Text;
+            int available = Convert.ToInt32(DropDownList6.Text);
+
+
+
+            string connstring = "host=127.0.0.1;port=3306;database=BloodBank;username=root;password=Praneet@1721";
+            MySqlConnection conn = new MySqlConnection(connstring);
+            conn.Open();
+            string query = $"insert Donors values({ID},'{name}','{BloodG}','{Mobile}','{country}','{state}','{district}','{city}','{Email}','{PassW}','{available}');";
+
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+
+            cmd.ExecuteNonQuery();
+
+            Response.Write("<script> alert('Registered succesfully');</script>");
+
+
+        }
+    }
+}
---
+++ search.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace WebApp2
+{
+    public partial class search : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+
+            String bg = DropDownList1.Text;
+            String country_name = DropDownList5.Text;
+            String state_name = DropDownList2.Text;
+            String district_name = DropDownList3.Text;
+            String city_name = DropDownList4.Text;
+
+            MySqlConnection conn = new MySqlConnection("host=localhost;username=root;password=Praneet@1721;database=BloodBank");
+            conn.Open();
+
+
+            GridView1.DataSource = null;
+            MySqlDataAdapter adapter = new MySqlDataAdapter($"select full_name, mobile_number,  availability_to_donate from Donors where (blood_group = '{bg}' AND  country ='{country_name}' AND state='{state_name}' AND district='{district_name}' AND city='{city_name}');", conn);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            conn.Close();
+        }
+    }
+}
